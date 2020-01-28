9366cf981df69aed6ab591826bfc28e02b40e153
Maria Belyi <mashabelyi@berkeley.edu>
2020-01-28 15:56:24 -0800

    updated preprocessing for OmniJR

--- BPL/data/omniglot_preprocess.m
+++ BPL/data/omniglot_preprocess.m
@@ -12,31 +12,9 @@
 %    data_evaluation_processed.mat
 %
 function omniglot_preprocess
-    fn_list = {'train','test'};
-    n = numel(fn_list);
-    for i=1:n
-       helper_process(fn_list{i}); 
-    end
     
-end
-
-function helper_process(input_type)
-
-    switch input_type        
-        case 'train'
-            is_train = true;
-        case 'test'
-            is_train = false;
-        otherwise
-            error('invalid type');
-    end
-
-    if is_train
-        load('data_background','drawings','images','names','timing');
-    else
-        load('data_evaluation','drawings','images','names','timing');
-    end
-
+    load('omniJr','drawings','images','names','timing');
+    
     % Parameters
     ps = defaultps_preprocess;
     tint = ps.tint;
@@ -56,27 +34,25 @@ function helper_process(input_type)
             end
         end
     end
-
+    
+    disp("Converting to uniform time");
     % Convert to uniform time
     ufunc = @(stk) uniform_time_lerp(stk(:,1:2),stk(:,3),tint);
     udrawings = apply_to_nested(drawings_aug,ufunc);
 
+    disp("Partitioning into sub strokes");
     % Partition into sub-strokes
     pfunc = @(stk) partition_strokes(stk,ps.dthresh,ps.max_sequence);
     pdrawings = apply_to_nested(udrawings,pfunc);
     
+    disp("Converting to uniform space");
     % Convert to uniform in space
     sfunc = @(stk) uniform_space_lerp(stk,ps.space_int);
     pdrawings = apply_to_nested(pdrawings,sfunc);
     
+    disp("Creating dataset");
     % Create dataset
     D = Dataset(pdrawings,images,names);
-
-    % save the results
-    if is_train
-        save('data_background_processed','D');
-    else
-        save('data_evaluation_processed','D');
-    end
     
+    save('data_background_processed','D');
 end
